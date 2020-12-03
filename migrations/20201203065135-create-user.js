5fec1b20276acf7865c87530692860c823491b04
satriayudhia <satria.yudhiap@gmail.com>
2020-12-03 16:33:12 +0700

    add transaction

--- migrations/20201203065135-create-user.js
+++ migrations/20201203065135-create-user.js
@@ -12,6 +12,11 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       status: {
